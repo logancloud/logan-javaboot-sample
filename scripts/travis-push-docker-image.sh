21a892725e627337a12ef0b72a19b9548de3bb03
linzhaoming <teleyic@gmail.com>
2019-10-09 13:55:10 +0800

    Travis test

--- scripts/travis-push-docker-image.sh
+++ scripts/travis-push-docker-image.sh
@@ -8,6 +8,11 @@ set -u
 
 export REPO="logancloud/logan-javaboot-sample"
 
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    docker push ${REPO}:latest
+fi
+
 if [[ "${TRAVIS_TAG}" != "" ]]; then
 	# For both git tags and git branches 'TRAVIS_BRANCH' contains the name.
     export TAG="${TRAVIS_BRANCH}"
